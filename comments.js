0f6039a3d8120dcff78e182280624ff88944f463
Leonardo Santos <rmnobarra@gmail.com>
2024-01-08 16:01:47 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,33 +1,37 @@
 // create web server
-// run node comments.js
-// open browser and go to http://localhost:3000
 
-var http = require('http');
-var url = require('url');
-var items = [];
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const morgan = require('morgan');
 
-var server = http.createServer(function (req, res) {
-    switch (req.method) {
-        case 'POST':
-            var item = '';
-            req.setEncoding('utf8');
-            req.on('data', function (chunk) {
-                item += chunk;
-            });
-            req.on('end', function () {
-                items.push(item);
-                res.end('OK\n');
-            });
-            break;
+// create express app
+const app = express();
 
-        case 'GET':
-            items.forEach(function (item, i) {
-                res.write(i + ') ' + item + '\n');
-            });
-            res.end();
-            break;
-    }
+// import routes
+const commentsRoutes = require('./routes/comments');
 
+// use morgan to log requests to the console
+app.use(morgan('dev'));
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// parse requests of content-type - application/json
+app.use(bodyParser.json());
+
+// enable cors
+app.use(cors());
+
+// define a simple route
+app.get('/', (req, res) => {
+  res.json({ message: 'Welcome to the comments application.' });
 });
 
-server.listen(3000);
+// use routes
+app.use('/comments', commentsRoutes);
+
+// listen for requests
+app.listen(3000, () => {
+  console.log('Server is listening on port 3000');
+});
